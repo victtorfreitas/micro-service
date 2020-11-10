fb680b61757ae4887a63a1fdd584cd6e4b5556c7
Victtor <victtorfreitas95@gmail.com>
2020-11-10 18:00:02 -0300

    Refes #1 Adiciona anotação service

--- loja/src/main/java/br/com/microservice/loja/loja/service/CompraService.java
+++ loja/src/main/java/br/com/microservice/loja/loja/service/CompraService.java
@@ -4,10 +4,12 @@ import br.com.microservice.loja.loja.controller.requestDTO.CompraRequest;
 import br.com.microservice.loja.loja.service.responseDTO.InfoFornecedorResponse;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.Objects;
 
+@Service
 public class CompraService {
 
     private static final String URL_INFO_FORNECEDOR = "http://localhost:8081/info/";
